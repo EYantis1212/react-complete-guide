6b97a00471998d01bf6a0755f9118fbd3c9b4a6a
yantis <yantis1212@outlook.com>
2022-02-11 15:48:24 -0700

    Add: (project 01) Refactor styling into Card comp

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -5,25 +5,25 @@ function App() {
 		{
 			id: 'e1',
 			title: 'Toilet Paper',
-			amount: 94.12,
+			amount: 12.12,
 			date: new Date(2020, 7, 14),
 		},
 		{
 			id: 'e2',
 			title: 'New TV',
-			amount: 799.49,
+			amount: 1799.49,
 			date: new Date(2021, 2, 12),
 		},
 		{
 			id: 'e3',
 			title: 'Car Insurance',
-			amount: 294.67,
+			amount: 94.67,
 			date: new Date(2021, 2, 28),
 		},
 		{
 			id: 'e4',
 			title: 'New Desk (Wooden)',
-			amount: 450,
+			amount: 450.45,
 			date: new Date(2021, 5, 12),
 		},
 	];
