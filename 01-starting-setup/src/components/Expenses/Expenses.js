ab9d68ecb3ad3a495532bc38c64b66cfe3104a8f
yantis <yantis1212@outlook.com>
2022-02-12 14:05:29 -0700

    Add: (project 01) Move logic into ExpensesList

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props) => {
 	console.log(props);
@@ -22,7 +22,7 @@ const Expenses = (props) => {
 					selectedYear={filterYear}
 					onFilterChange={filterChangeHandler}
 				/>
-				{expensesContent}
+				<ExpensesList items={filteredExpenses} />
 			</Card>
 		</div>
 	);
